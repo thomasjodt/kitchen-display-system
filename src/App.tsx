e382d49ff227b42b04685032ced2916779c6b8df
thomasjodt <jodt5200@gmail.com>
2024-05-21 19:55:26 -0500

    feat(root): Add dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { styled } from 'styled-components'
 export const App: React.FC = function App () {
-  return <div>hola, mundo</div>
+  return (
+    <Wrapper>
+      hola, mundo
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.main`
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+`

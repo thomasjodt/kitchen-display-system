5aacb3b86b5d555600c515555c2301d7e6d51962
thomasjodt <jodt5200@gmail.com>
2024-05-21 20:07:57 -0500

    feat(app): Add store to the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 
 import { App } from './App.tsx'
+import { store } from './context'
 import './index.css'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )

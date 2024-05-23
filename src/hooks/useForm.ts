24b08524e4a9fd5fc53652afd3f493683408a3b4
thomasjodt <jodt5200@gmail.com>
2024-05-23 00:05:19 -0500

    refactor(hooks): Add interface for handleChange method to accept more than one target

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 
+type FormElement = HTMLInputElement | HTMLSelectElement
 interface UseForm<T> {
   form: T
   reset: () => void
-  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleChange: (e: React.ChangeEvent<FormElement>) => void
+}
+
+interface HandleChange {
+  (e: React.ChangeEvent<HTMLInputElement>): void
+  (e: React.ChangeEvent<HTMLSelectElement>): void
+  (e: React.ChangeEvent<FormElement>): void
 }
 
 export const useForm = <T>(initialForm: T): UseForm<T> => {
   const [form, setForm] = useState<T>(initialForm)
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleChange: HandleChange = (e) => {
     const { name, value } = e.target
     setForm(f => ({ ...f, [name]: value }))
   }

3efb2d30b80234f938882b3830e2cf7d49cae02f
thomasjodt <jodt5200@gmail.com>
2024-05-22 19:20:27 -0500

    feat(components/ui): Create ProductElement component

---
+++ src/components/ui/ProductElement.tsx
@@ -0,0 +1,35 @@
+import type { Product } from '@/types'
+import styled from 'styled-components'
+
+interface Props {
+  product: Product
+}
+
+export const ProductElement: React.FC<Props> = function ({ product }) {
+  return (
+    <Container>
+      <section>
+        <p>{product.name}</p>
+        <Comment>{product?.comment}</Comment>
+      </section>
+      <p>{product.quantity}</p>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  margin-bottom: 1px solid #000;
+  display: flex;
+  justify-content: space-between;
+  padding-inline: 12px;
+  font-weight: 500;
+  background-color: #CED6DF;
+  border-radius: 8px;
+  margin-inline: 12px;
+`
+const Comment = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  color: #75A5BD;
+
+`

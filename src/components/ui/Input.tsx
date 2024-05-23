c36d643538ec69395d3ccb6d55369146147130a5
thomasjodt <jodt5200@gmail.com>
2024-05-23 00:07:53 -0500

    refactor(components/ui): Improve style to accept a button inside and an event that can be called with 'Alt' + 'Enter'

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,26 +1,56 @@
+import React from 'react'
 import styled from 'styled-components'
+import { Button } from './Button'
 
-interface Props {
+interface Props extends React.HTMLProps<HTMLInputElement> {
   label?: string
   name: string
-  placeholder?: string
   value?: string
+  buttonText?: string
+  buttonAction?: (value: string) => void
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input: React.FC<Props> = function ({ label, name, placeholder, value, onChange }) {
+export const Input: React.FC<Props> = function ({ label, name, placeholder, value, buttonText, buttonAction, onChange }) {
+  const handleClick = (): void => {
+    if (buttonAction !== undefined && value !== undefined) {
+      buttonAction(value)
+    }
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === 'Enter') e.preventDefault()
+
+    if (e.key === 'Enter' && e.ctrlKey) {
+      if (buttonAction !== undefined && value !== undefined) {
+        buttonAction(value)
+      }
+    }
+  }
+
   return (
-    <div>
+    <>
       {(label !== null) && <Label htmlFor={name}>{label}</Label>}
-      <TextInput
-        type='text'
-        name={name}
-        id={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
+      <Div>
+        <TextInput
+          type='text'
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+        />
+        {(buttonText !== undefined) && (
+          <Button
+            type='button'
+            text={buttonText}
+            style={{ margin: 4 }}
+            onClick={handleClick}
+          />
+        )}
+      </Div>
+    </>
   )
 }
 
@@ -31,17 +61,24 @@ const Label = styled.label`
   font-size: 14px;
 `
 
-const TextInput = styled.input`
+const Div = styled.div`
+  display: flex;
   border-radius: 4px;
-  padding: 12px;
   border: 1px solid #d7d7d7;
   font-size: 16px;
   outline: none;
   margin-bottom: 12px;
   width: 100%;
 
-  &:focus {
-    outline: 2px solid rgba(18, 111, 218, 0.5);
+  &:has(input:focus) {
     border-color: transparent;
+    box-shadow: 0 0 0 2px rgba(18, 111, 218, 0.4);
   }
+`
+const TextInput = styled.input`
+  flex-grow: 1;
+  padding: 12px;
+  border: none;
+  outline: none;
+  border-radius: 4px;
 `
